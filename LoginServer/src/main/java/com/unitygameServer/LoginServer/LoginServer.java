b9fa506d19dd7438a51f6407a23a1de4bf44997d
zjy <2910553808@qq.com>
2022-06-11 17:15:31 +0800

    提交修改

--- LoginServer/src/main/java/com/unitygameServer/LoginServer/LoginServer.java
+++ LoginServer/src/main/java/com/unitygameServer/LoginServer/LoginServer.java
@@ -1,14 +1,12 @@
 package com.unitygameServer.LoginServer;
 
-import com.zfoo.orm.OrmContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginServer {
-    public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(LoginServer.class);
+    public static void main(String[] args) throws NullPointerException{
+
     }
 }
