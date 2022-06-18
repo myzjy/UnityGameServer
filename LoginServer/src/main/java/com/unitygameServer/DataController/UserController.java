18d3049df82a056f7e493d383e940760a3d3324e
zjy <2910553808@qq.com>
2022-06-18 12:06:49 +0800

    添加模型

---
+++ LoginServer/src/main/java/com/unitygameServer/DataController/UserController.java
@@ -0,0 +1,21 @@
+package com.unitygameServer.DataController;
+
+import com.unitygameServer.model.UserModel;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+/**
+ * 用户控制器
+ * @author zjy
+ * @version 0.1
+ */
+public class UserController {
+    /**
+     *  用户数据
+     * */
+    public Dictionary<Integer, UserModel> UserModelDict=new Hashtable<>();
+
+    public void Init() {
+
+    }
+}
--- LoginServer/src/main/java/com/unitygameServer/LoginServer/LoginServer.java
+++ LoginServer/src/main/java/com/unitygameServer/LoginServer/LoginServer.java
@@ -1,12 +1,44 @@
 package com.unitygameServer.LoginServer;
 
+import com.zfoo.event.model.event.AppStartEvent;
+import com.zfoo.net.core.websocket.WebsocketServer;
+import com.zfoo.util.net.HostAndPort;
+import com.zfoo.util.net.NetUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.stereotype.Controller;
+@SpringBootApplication(exclude = {
+        // 排除MongoDB自动配置
+        MongoDataAutoConfiguration.class,
+        MongoRepositoriesAutoConfiguration.class,
+        MongoAutoConfiguration.class,
+        MongoReactiveAutoConfiguration.class,
+        MongoReactiveDataAutoConfiguration.class,
+        MongoReactiveRepositoriesAutoConfiguration.class,
 
-@Controller
+        TaskExecutionAutoConfiguration.class,
+        TaskSchedulingAutoConfiguration.class
+})
 public class LoginServer {
 
-    public static void main(String[] args) throws NullPointerException{
+    public static final int WEBSOCKET_SERVER_PORT = 8080;
 
+
+    public static void main(String[] args) throws NullPointerException {
+        var springApp = SpringApplication.run(LoginServer.class,args);
+        springApp.registerShutdownHook();
+        springApp.publishEvent(new AppStartEvent(springApp));
+        // 启动websocket
+        var websocket = new WebsocketServer(HostAndPort.valueOf(NetUtils.getLocalhostStr(), WEBSOCKET_SERVER_PORT));
+        websocket.start();
     }
 }
