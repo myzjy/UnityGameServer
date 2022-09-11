c3389098a9cbfd26e5b83763dd1bf6636bbb193a
zjy <2910553808@qq.com>
2022-09-12 01:01:07 +0800

    设置注册玩家token

--- LoginServer/src/main/java/com/unitygameServer/serverGame/singleServer/DataController/LoginController.java
+++ LoginServer/src/main/java/com/unitygameServer/serverGame/singleServer/DataController/LoginController.java
@@ -1,16 +1,17 @@
 package com.unitygameServer.serverGame.singleServer.DataController;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.unitygameServer.serverGame.commonRefush.constant.I18nEnum;
 import com.unitygameServer.serverGame.commonRefush.constant.TankDeployEnum;
 import com.unitygameServer.serverGame.commonRefush.entity.AccountEntity;
 import com.unitygameServer.serverGame.commonRefush.entity.UserEntity;
 import com.unitygameServer.serverGame.commonRefush.protocol.login.LoginRequest;
-import com.unitygameServer.serverGame.singleServer.model.UserModel;
 import com.zfoo.event.manager.EventBus;
 import com.zfoo.net.NetContext;
 import com.zfoo.net.packet.common.Error;
 import com.zfoo.net.router.receiver.PacketReceiver;
-import com.zfoo.net.session.model.AttributeType;
 import com.zfoo.net.session.model.Session;
 import com.zfoo.orm.OrmContext;
 import com.zfoo.orm.model.anno.EntityCachesInjection;
@@ -24,8 +25,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
+import java.util.Calendar;
+
 
 /**
  * 登录控制器
@@ -59,55 +60,63 @@ public class LoginController {
             //传递过来的账号不对
             //信息传递给客户端
             NetContext.getRouter().send(session, Error.valueOf(I18nEnum.error_account_not_exit.toString()));
-
             return;
         }
-//此处为校验敏感词
-//        if () {
-//
-//        }
 
         var sid = session.getSid();
-        EventBus.execute(HashUtils.fnvHash(account), new Runnable() {
-            @Override
-            public void run() {
-                //数据库拿去
-                var userEntity = OrmContext.getAccessor().load(account, AccountEntity.class);
-                if (userEntity == null) {
-                    //没找到 生成新的uid uid只会在创建角色了会出现
-                    var newUID = MongoIdUtils.getIncrementIdFromMongoDefault(UserEntity.class);
-                    userEntity = AccountEntity.valueOf(account, account, password, -1);
-                    //插入数据库
-                    OrmContext.getAccessor().insert(userEntity);
-                    OrmContext.getAccessor().insert(UserEntity.valueOf(newUID, account, TimeUtils.now(), TimeUtils.now()));
+        EventBus.execute(HashUtils.fnvHash(account), () -> {
+            //数据库拿去
+            var userEntity = OrmContext.getAccessor().load(account, AccountEntity.class);
+            if (userEntity == null) {
+
+//                    OrmContext.getOrmManager().getCollection();
+                //没找到 生成新的uid uid只会在创建角色了会出现
+                var newUID = MongoIdUtils.getIncrementIdFromMongoDefault(UserEntity.class) + 10000000;
+                userEntity = AccountEntity.valueOf(account, account, password, newUID);
+
+
+                //插入数据库
+                OrmContext.getAccessor().insert(userEntity);
+                UserEntity user=UserEntity.valueOf(newUID, account, TimeUtils.now(), TimeUtils.now());
+                user.setToken(getToken(userEntity));
+                OrmContext.getAccessor().insert(user);
 
 //                    OrmContext.getAccessor().update(userEntity);
-                }
-                if (deployEnum == TankDeployEnum.dev) {
-                    //验证密码
-                    if (StringUtils.isNotBlank(userEntity.getPassword()) && !userEntity.getPassword().trim().equals(password.trim())) {
-                        if (userEntity.getUid() > 0) {
-                            logger.info("[password:{}]账号或密码错误", password);
-                        } else {
-                            logger.info("[uid:{}][password:{}]账号或密码错误", userEntity.getUid(), password);
-                        }
-                        //给客户端服务器
-                        NetContext.getRouter().send(session, Error.valueOf(I18nEnum.error_account_password.toString()));
-                        return;
-                    }
-                }
-                var uid = userEntity.getUid();
-
-                logger.info("[{}][{}]玩家登录[account:{}][password:{}]", uid, sid, account, password);
-
-                var player = UserModelDict.load(uid);
-                player.setLastLoginTime(TimeUtils.now());
-
-                player.sid = sid;
-                player.session = session;
-//                session
             }
+            if (deployEnum == TankDeployEnum.dev) {
+                //验证密码
+                if (StringUtils.isNotBlank(userEntity.getPassword()) && !userEntity.getPassword().trim().equals(password.trim())) {
+                    if (userEntity.getUid() > 0) {
+                        logger.info("[password:{}]账号或密码错误", password);
+                    } else {
+                        logger.info("[uid:{}][password:{}]账号或密码错误", userEntity.getUid(), password);
+                    }
+                    //给客户端服务器
+                    NetContext.getRouter().send(session, Error.valueOf(I18nEnum.error_account_password.toString()));
+                    return;
+                }
+            }
+            var uid = userEntity.getUid();
+
+            logger.info("[{}][{}]玩家登录[account:{}][password:{}]", uid, sid, account, password);
+
+            var player = UserModelDict.load(uid);
+            player.setLastLoginTime(TimeUtils.now());
+
+            player.sid = sid;
+            player.session = session;
+//                session
         });
+    }
+
+    public static String getToken(AccountEntity account) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, 10);
+        JWTCreator.Builder builder = JWT.create();
+        builder.withClaim("id", account.getId()).withClaim("UID", account.getUid()).withClaim("password", account.getPassword());
+        String token = builder.withExpiresAt(calendar.getTime()).sign(Algorithm.HMAC256(sun.security.x509.X509CertImpl.SIGNATURE));
+        logger.info("[token:{}]",token);
+        return token;
     }
 
 //    @PacketReceiver
