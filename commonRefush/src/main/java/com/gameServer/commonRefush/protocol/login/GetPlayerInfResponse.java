7b549c33944b848114ef31f80e2c9f167bc87976
zjy <2910553808@qq.com>
2022-10-27 00:25:28 +0800

    完善登录

---
+++ commonRefush/src/main/java/com/gameServer/commonRefush/protocol/login/GetPlayerInfResponse.java
@@ -0,0 +1,13 @@
+package com.gameServer.commonRefush.protocol.login;
+
+import com.zfoo.protocol.IPacket;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @since 2022/10/26 14:22
+ */
+public class GetPlayerInfResponse implements IPacket{
+    public static final transient short PROTOCOL_ID = 1005;
+
+}
