5af50710aeb2e47d57ba41aceacdfac04125902f
zjy <2910553808@qq.com>
2022-11-02 00:42:54 +0800

    完善体系

--- game-boot/src/main/java/com/gameServer/gameBoot/controller/RegisterController.java
+++ game-boot/src/main/java/com/gameServer/gameBoot/controller/RegisterController.java
@@ -2,13 +2,13 @@ package com.gameServer.gameBoot.controller;
 
 import com.gameServer.commonRefush.result.BaseResponse;
 import com.gameServer.commonRefush.result.CodeEnum;
+import com.gameServer.gameBoot.model.register.RegisterDataModelRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Administrator
@@ -19,10 +19,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @CrossOrigin
 public class RegisterController {
     private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
-    @PostMapping("/api/register")
+    @PostMapping(value = "/api/register")
     @ResponseBody
-    public BaseResponse getResVersion() {
+    public BaseResponse getResVersion(HttpServletRequest request, @RequestBody RegisterDataModelRequest cm) {
         logger.info("/api/getVersion");
+        logger.info(cm.platformId);
         return BaseResponse.valueOf(CodeEnum.OK);
     }
 }
---
+++ game-boot/src/main/java/com/gameServer/gameBoot/model/register/RegisterDataModelRequest.java
@@ -0,0 +1,13 @@
+package com.gameServer.gameBoot.model.register;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @since 2022/10/31 12:42
+ */
+public class RegisterDataModelRequest {
+    public String platformId;
+    public String platfromToken;
+    public String channelCode;
+    public String version;
+}
