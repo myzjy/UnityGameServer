d40e52aafe523d5d4ce9ab1e6fa94dc8a2179f25
zjy <2910553808@qq.com>
2022-11-30 00:00:36 +0800

    背包注册

--- commonRefush/src/test/java/ApplicationTest.java
+++ commonRefush/src/test/java/ApplicationTest.java
@@ -1,16 +1,21 @@
+import com.gameServer.common.test.bagTest.BagTest;
+import org.junit.Ignore;
+import org.junit.Test;
+
 /**
  * @author Administrator
  * @version 1.0
  * @since 2022/11/20 1:45
  */
+@Ignore
 public class ApplicationTest {
 
     /**
      * 将表放到数据库中
      */
-//    @Test
-//    public void ApplyDataMain() {
-//        BagTest _bag = new BagTest();
-//        _bag.BagTest();
-//    }
+    @Test
+    public void ApplyDataMain() {
+        BagTest _bag = new BagTest();
+        _bag.BagTest();
+    }
 }
