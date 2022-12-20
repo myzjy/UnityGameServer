d164b51daa5a983cb815dbdd373da6136c78134c
zjy <2910553808@qq.com>
2022-12-21 00:14:26 +0800

    房间

---
+++ game-room/src/main/java/com/gameServer/gameRoom/boomEvent/createRoom/controller/CreateRoomController.java
@@ -0,0 +1,16 @@
+package com.gameServer.gameRoom.boomEvent.createRoom.controller;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 创建房间控制器
+ *
+ * @author zjy
+ * @version 1.0
+ * @since 2022/12/20 23:39
+ */
+@Component
+public class CreateRoomController {
+
+
+}
