f09470b3a9b874af4c2ab2c99a966a8b82688ccd
zjy <2910553808@qq.com>
2023-03-30 00:03:36 +0800

    体力系统v1.0

--- GameServer/src/main/java/com/gameServer/singleServer/user/controller/UserLoginController.java
+++ GameServer/src/main/java/com/gameServer/singleServer/user/controller/UserLoginController.java
@@ -1,6 +1,7 @@
 package com.gameServer.singleServer.user.controller;
 
 import com.gameServer.commonRefush.constant.I18nEnum;
+import com.gameServer.commonRefush.entity.PhysicalPowerEntity;
 import com.gameServer.commonRefush.entity.PlayerUserEntity;
 import com.gameServer.commonRefush.protocol.cache.refresh.RefreshLoginPhysicalPowerNumAnswer;
 import com.gameServer.commonRefush.protocol.cache.refresh.RefreshLoginPhysicalPowerNumAsk;
@@ -30,6 +31,8 @@ public class UserLoginController {
      */
     @EntityCachesInjection
     private IEntityCaches<Long, PlayerUserEntity> UserModelDict;
+    @EntityCachesInjection
+    private IEntityCaches<Long, PhysicalPowerEntity> physicalPowerEntityIEntityCaches;
 
     @PacketReceiver
     public void atRefreshLoginPhysicalPowerNumAsk(Session session, RefreshLoginPhysicalPowerNumAsk numAsk) {
@@ -39,6 +42,22 @@ public class UserLoginController {
             NetContext.getRouter().send(session, RefreshLoginPhysicalPowerNumAnswer.ValueOf(Error.valueOf(numAsk, I18nEnum.error_account_not_exit.toString())));
             return;
         }
+        var data = physicalPowerEntityIEntityCaches.load(numAsk.getUserId());
+        if (data == null) {
+            logger.error("[uid:{}]体力缓存数据库不存在，请创建，流程有问题", numAsk.getUserId());
+            NetContext.getRouter().send(session, RefreshLoginPhysicalPowerNumAnswer.ValueOf(Error.valueOf(numAsk, I18nEnum.error_login_process_not.toString())));
+            return;
+        }
+        //当前体力
+        var nowPhysicalPower = data.getNowPhysicalPowerNum();
+        if (nowPhysicalPower >= data.getMaximumStrength()) {
+            logger.info("[uid:{}]体力已满[当前体力：{}] [目前等级为止的最大体力：{}]", numAsk.getUserId(), nowPhysicalPower, data.getMaximumStrength());
+            //体力满了
+            NetContext.getRouter().send(session, RefreshLoginPhysicalPowerNumAnswer.ValueOf(userEntity));
+        }
+
+
+        logger.info("[uid:{}]体力回复，[当前体力：{}] [目前等级为止的最大体力：{}]", numAsk.getUserId(), nowPhysicalPower, data.getMaximumStrength());
         NetContext.getRouter().send(session, RefreshLoginPhysicalPowerNumAnswer.ValueOf(userEntity));
     }
 }
