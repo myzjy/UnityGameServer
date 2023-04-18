b24752bfd87f78a6414cdf45d70dbf31aebf4b24
zjy <2910553808@qq.com>
2023-04-18 19:38:10 +0800

    添加

--- GameServer/src/main/java/com/gameServer/singleServer/PhysicalPower/controller/PhysicalPowerUsePropsController.java
+++ GameServer/src/main/java/com/gameServer/singleServer/PhysicalPower/controller/PhysicalPowerUsePropsController.java
@@ -1,9 +1,18 @@
 package com.gameServer.singleServer.PhysicalPower.controller;
 
+import com.gameServer.commonRefush.entity.PhysicalPowerEntity;
+import com.gameServer.commonRefush.entity.PlayerUserEntity;
+import com.gameServer.commonRefush.protocol.cache.create.CreatePhysicalPowerAnswer;
+import com.gameServer.commonRefush.protocol.cache.create.CreatePhysicalPowerAsk;
+import com.gameServer.commonRefush.protocol.physicalPower.PhysicalPowerRequest;
+import com.gameServer.commonRefush.protocol.physicalPower.PhysicalPowerResponse;
 import com.gameServer.commonRefush.protocol.physicalPower.PhysicalPowerUsePropsRequest;
 import com.gameServer.singleServer.PhysicalPower.service.IPhysicalPowerService;
+import com.zfoo.net.NetContext;
+import com.zfoo.net.packet.common.Error;
 import com.zfoo.net.router.receiver.PacketReceiver;
 import com.zfoo.net.session.Session;
+import com.zfoo.orm.OrmContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +50,48 @@ public class PhysicalPowerUsePropsController {
         } else {
             logger.info("当前扣除体力值：{}，扣除体力,当前体力：{},扣除完体力值：{}", request.getUsePropNum(), physicalData.getNowPhysicalPowerNum(), physicalReduce);
             //需要将体力相关修改
+
         }
 
+    }
+
+    @PacketReceiver
+    public void atPhysicalPowerRequest(Session session, PhysicalPowerRequest request) throws NullPointerException {
+        logger.info("[uid:{}] 获取体力 atPhysicalPowerRequest", session.getUid());
+        //获取到服务器 数据库存放
+        var data = OrmContext.getAccessor().load(session.getUid(), PhysicalPowerEntity.class);
+        if (data == null) {
+            var serverSession = NetContext.getSessionManager().getServerSession(1);
+            var user = OrmContext.getAccessor().load(session.getUid(), PlayerUserEntity.class);
+            if (user == null) {
+                logger.error("数据库错误，传递错误uid 错误的[uid:{}] ", session.getUid());
+                NetContext.getRouter().send(session, Error.valueOf("数据库错误，传递错误uid"));
+                return;
+            }
+            logger.error("[uid:{}] 获取体力 时,数据库相关不存在，开始创建", session.getUid());
+
+            //这种情况一般不会有的，如果有那就rpc通信去创建
+            NetContext.getRouter().asyncAsk(serverSession,
+                    CreatePhysicalPowerAsk.ValueOf(user.getPlayerLv(), user.getId()),
+                    CreatePhysicalPowerAnswer.class, null).whenComplete(res -> {
+                //体力重新创建出来了，返回出去
+                var createData = OrmContext.getAccessor().load(session.getUid(), PhysicalPowerEntity.class);
+                if (createData == null) {
+                    logger.error("[uid:{}] 获取体力 时,数据库错误，创建数据错误", session.getUid());
+                    NetContext.getRouter().send(session, Error.valueOf("数据库错误，创建数据错误，请联系客服"));
+                    return;
+                }
+                logger.info("[uid:{}] 获取体力 时,数据库相关不存在，创建成功", session.getUid());
+
+                //有了数据传递过去
+                NetContext.getRouter().send(session, PhysicalPowerResponse.ValueOf(createData.getNowPhysicalPowerNum(), createData.getResidueTime(),
+                        createData.getMaximumStrength(), createData.getMaximusResidueEndTime(), createData.getResidueNowTime()));
+            });
+        } else {
+            logger.info("[uid:{}] 获取体力 完成", session.getUid());
+            //有了数据传递过去
+            NetContext.getRouter().send(session, PhysicalPowerResponse.ValueOf(data.getNowPhysicalPowerNum(), data.getResidueTime(),
+                    data.getMaximumStrength(), data.getMaximusResidueEndTime(), data.getResidueNowTime()));
+        }
     }
 }
--- GameServer/src/main/java/com/gameServer/singleServer/login/controller/LoginTapStartController.java
+++ GameServer/src/main/java/com/gameServer/singleServer/login/controller/LoginTapStartController.java
@@ -2,18 +2,9 @@ package com.gameServer.singleServer.login.controller;
 
 import com.gameServer.commonRefush.constant.TankDeployEnum;
 import com.gameServer.commonRefush.entity.AccessGameTimeEntity;
-import com.gameServer.commonRefush.entity.PhysicalPowerEntity;
-import com.gameServer.commonRefush.entity.PlayerUserEntity;
-import com.gameServer.commonRefush.event.create.CreateOrmAccesTimeEvent;
-import com.gameServer.commonRefush.protocol.cache.create.CreatePhysicalPowerAnswer;
-import com.gameServer.commonRefush.protocol.cache.create.CreatePhysicalPowerAsk;
 import com.gameServer.commonRefush.protocol.login.LoginTapToStartRequest;
 import com.gameServer.commonRefush.protocol.login.LoginTapToStartResponse;
-import com.gameServer.commonRefush.protocol.physicalPower.PhysicalPowerRequest;
-import com.gameServer.commonRefush.protocol.physicalPower.PhysicalPowerResponse;
-import com.zfoo.event.model.anno.EventReceiver;
 import com.zfoo.net.NetContext;
-import com.zfoo.net.packet.common.Error;
 import com.zfoo.net.router.receiver.PacketReceiver;
 import com.zfoo.net.session.Session;
 import com.zfoo.orm.OrmContext;
@@ -58,45 +49,7 @@ public class LoginTapStartController {
         }
     }
 
-    @PacketReceiver
-    public void atPhysicalPowerRequest(Session session, PhysicalPowerRequest request) throws NullPointerException {
-        logger.info("[uid:{}] 获取体力 atPhysicalPowerRequest", session.getUid());
-        //获取到服务器 数据库存放
-        var data = OrmContext.getAccessor().load(session.getUid(), PhysicalPowerEntity.class);
-        if (data == null) {
-            var serverSession = NetContext.getSessionManager().getServerSession(1);
-            var user = OrmContext.getAccessor().load(session.getUid(), PlayerUserEntity.class);
-            if (user == null) {
-                logger.error("数据库错误，传递错误uid 错误的[uid:{}] ", session.getUid());
-                NetContext.getRouter().send(session, Error.valueOf("数据库错误，传递错误uid"));
-                return;
-            }
-            logger.error("[uid:{}] 获取体力 时,数据库相关不存在，开始创建", session.getUid());
 
-            //这种情况一般不会有的，如果有那就rpc通信去创建
-            NetContext.getRouter().asyncAsk(serverSession,
-                    CreatePhysicalPowerAsk.ValueOf(user.getPlayerLv(), user.getId()),
-                    CreatePhysicalPowerAnswer.class, null).whenComplete(res -> {
-                //体力重新创建出来了，返回出去
-                var createData = OrmContext.getAccessor().load(session.getUid(), PhysicalPowerEntity.class);
-                if (createData == null) {
-                    logger.error("[uid:{}] 获取体力 时,数据库错误，创建数据错误", session.getUid());
-                    NetContext.getRouter().send(session, Error.valueOf("数据库错误，创建数据错误，请联系客服"));
-                    return;
-                }
-                logger.info("[uid:{}] 获取体力 时,数据库相关不存在，创建成功", session.getUid());
-
-                //有了数据传递过去
-                NetContext.getRouter().send(session, PhysicalPowerResponse.ValueOf(createData.getNowPhysicalPowerNum(), createData.getResidueTime(),
-                        createData.getMaximumStrength(), createData.getMaximusResidueEndTime(), createData.getResidueNowTime()));
-            });
-        } else {
-            logger.info("[uid:{}] 获取体力 完成", session.getUid());
-            //有了数据传递过去
-            NetContext.getRouter().send(session, PhysicalPowerResponse.ValueOf(data.getNowPhysicalPowerNum(), data.getResidueTime(),
-                    data.getMaximumStrength(), data.getMaximusResidueEndTime(), data.getResidueNowTime()));
-        }
-    }
 
 
 }
