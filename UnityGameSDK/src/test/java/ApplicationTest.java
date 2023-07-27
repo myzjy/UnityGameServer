77a91c7cc4bd599fc280b9acb83c0d6cde065591
zjy <2910553808@qq.com>
2023-07-27 17:59:44 +0800

    添加rpc设置，将写入数据库代码移动到其他模块

---
+++ UnityGameSDK/src/test/java/ApplicationTest.java
@@ -0,0 +1,38 @@
+import com.gameServer.commonRefush.constant.TankDeployEnum;
+import com.zfoo.event.model.event.AppStartEvent;
+import com.zfoo.util.ThreadUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author zjy
+ * @version 1.0
+ * @since 2023/4/18 19 44
+ */
+@Ignore
+public class ApplicationTest {
+    static {
+        TankDeployEnum.InitDefaultEnv();
+    }
+
+
+    @Test
+    public void startApplication1() {
+        Application.main(null);
+
+        ThreadUtils.sleep(Long.MAX_VALUE);
+    }
+
+    @Test
+    public void TestEx() {
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        context.registerShutdownHook();
+        context.publishEvent(new AppStartEvent(context));
+        var studentManager = context.getBean(PuzzleManager.class);
+        studentManager.UpDataPuzzleOrm();
+        var ormAddManager = context.getBean(OrmAddManager.class);
+        ormAddManager.UpdateItemBaseCsvResource();
+
+    }
+}
---
+++ UnityGameSDK/src/test/java/PuzzleManager.java
@@ -0,0 +1,55 @@
+import com.gameServer.commonRefush.entity.PuzzleEntity;
+import com.gameServer.commonRefush.resource.PuzzleResource;
+import com.zfoo.orm.OrmContext;
+import com.zfoo.scheduler.util.TimeUtils;
+import com.zfoo.storage.model.anno.ResInjection;
+import com.zfoo.storage.model.vo.Storage;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zjy
+ * @version 1.0
+ * @since 2023/7/26 10 37
+ */
+@Component
+public class PuzzleManager {
+    @ResInjection
+    public Storage<Integer, PuzzleResource> puzzleResourceStorage;
+
+    public void UpDataPuzzleOrm() {
+        var dict = puzzleResourceStorage.getAll();
+        for (var data :
+                dict) {
+            var dataEntity = OrmContext.getAccessor().load(data.getId(), PuzzleEntity.class);
+            if (dataEntity == null) {
+                var entity = PuzzleEntity.ValueOf();
+                entity.setId(data.getId());
+                entity.setPuzzleName(data.getPuzzleName());
+                entity.setPuzzleRewards(data.getPuzzleRewards());
+                entity.setLastPuzzleID(data.getLastPuzzleID());
+                entity.setNextPuzzleID(data.getNextPuzzleID());
+                entity.setAndroidVersion(data.getAndroidVersion());
+                entity.setIosVersion(data.getIosVersion());
+                entity.setPcEditorVersion(data.getPcEditorVersion());
+                var update = TimeUtils.timeToString(TimeUtils.now());
+
+                entity.setCreateAt(update);
+                entity.setUpdatedAt(update);
+                OrmContext.getAccessor().insert(entity);
+            } else {
+                var entity = PuzzleEntity.ValueOf();
+                entity.setId(data.getId());
+                entity.setPuzzleName(data.getPuzzleName());
+                entity.setPuzzleRewards(data.getPuzzleRewards());
+                entity.setLastPuzzleID(data.getLastPuzzleID());
+                entity.setNextPuzzleID(data.getNextPuzzleID());
+                entity.setAndroidVersion(dataEntity.getAndroidVersion());
+                entity.setIosVersion(dataEntity.getIosVersion());
+                entity.setPcEditorVersion(dataEntity.getPcEditorVersion());
+                var update = TimeUtils.timeToString(TimeUtils.now());
+                entity.setUpdatedAt(update);
+                OrmContext.getAccessor().update(entity);
+            }
+        }
+    }
+}
