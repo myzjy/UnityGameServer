76da8aff13a4bfb4d3b3c430a4705fe6b185ecc3
‘zjy’ <2910553808@qq.com>
2023-08-10 20:00:42 +0800

    关卡配置表和类第一版v0.0.2提交

---
+++ ampieta-game-java-common/src/main/java/com/gameServer/commonRefush/constant/PuzzleRewardsEnum.java
@@ -0,0 +1,29 @@
+package com.gameServer.commonRefush.constant;
+
+/**
+ * 通关奖励 因为 完成关卡任务奖励就已经有钻石了
+ * 
+ *
+ * @author zjy
+ * @version 1.0
+ * @since 2023/8/10 17 07
+ */
+public enum PuzzleRewardsEnum {
+    /**
+     * 金币
+     */
+    GoldCoin(1),
+    /**
+     * 装备
+     */
+    Equipment(2),
+    ;
+    /**
+     * type 数字
+     */
+    public int type;
+
+    PuzzleRewardsEnum(int type) {
+        this.type = type;
+    }
+}
