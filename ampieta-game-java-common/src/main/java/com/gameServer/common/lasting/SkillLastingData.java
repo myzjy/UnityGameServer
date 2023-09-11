f27e031ef4ad595883b13a358e535b043feac3bb
zjy <2910553808@qq.com>
2023-09-12 00:11:58 +0800

    导入包修改

---
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/lasting/SkillLastingData.java
@@ -0,0 +1,10 @@
+package com.gameServer.common.lasting;
+
+/**
+ * @author zjy
+ * @version 1.0
+ * @since 2023/9/12 00 11
+ */
+public class SkillLastingData {
+    private int skillId;
+}
---
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/skill/SkillData.java
@@ -0,0 +1,10 @@
+package com.gameServer.common.protocol.skill;
+
+/**
+ * 技能
+ * @author zjy
+ * @version 1.0
+ * @since 2023/9/12 00 04
+ */
+public class SkillData {
+}
