16a19712be0c11cde4591ad21b79bb6a7535ea7b
‘zjy’ <2910553808@qq.com>
2023-09-12 19:20:43 +0800

    调用错误包 改成 调用正确的包

--- ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/skill/SkillData.java
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/skill/SkillData.java
@@ -9,8 +9,22 @@ import com.zfoo.net.packet.IPacket;
  * @since 2023/9/12 00 04
  */
 public class SkillData  implements IPacket {
+    /**
+     * 技能ID 技能id不会重复
+     */
     private int skillID;
+    /**
+     * 技能名字
+     */
     private String skillName;
+    /**
+     * 技能介绍
+     */
     private String skillDesc;
+    /**
+     * 技能type
+     * 表示 技能类别
+     * 对敌人(全体、单体、debuff[已方、敌方]),对已方(只能对自己、已方[单体、全体]、buff[只能单体、全体])
+     */
     private int skillType;
 }
