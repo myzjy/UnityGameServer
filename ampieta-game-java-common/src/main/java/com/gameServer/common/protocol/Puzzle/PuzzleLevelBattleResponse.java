5c4f810e32c7ad103f81c6d1bcec00853c02c3c3
zjy <2910553808@qq.com>
2023-09-14 00:01:44 +0800

    导入包修改

--- ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/Puzzle/PuzzleLevelBattleResponse.java
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/Puzzle/PuzzleLevelBattleResponse.java
@@ -2,12 +2,14 @@ package com.gameServer.common.protocol.Puzzle;
 
 import com.gameServer.common.attribute.grounding.MonsterData;
 import com.zfoo.net.packet.IPacket;
+import com.zfoo.protocol.anno.Protocol;
 
 /**
  * @author zjy
  * @version 1.0
  * @since 2023/9/8 16 20
  */
+@Protocol(id = 1038)
 public class PuzzleLevelBattleResponse implements IPacket {
     /**
      * 当前关卡名字
