c3875c596602f106e583d65b5a4f9b7d4ab9b077
zjy <2910553808@qq.com>
2023-09-21 23:49:42 +0800

    角色数据

--- ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/equipment/EquipmentData.java
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/equipment/EquipmentData.java
@@ -1,9 +1,29 @@
 package com.gameServer.common.protocol.equipment;
 
+import com.zfoo.net.packet.IPacket;
+
 /**
  * @author zjy
  * @version 1.0
  * @since 2023/9/20 23 49
  */
-public class EquipmentData {
+public class EquipmentData implements IPacket {
+    /**
+     * 主属性 词条
+     */
+    private EquipmentGlossaryData SubjectClauseEquipmentData;
+    /**
+     * 副 词条 多个词条
+     */
+    private EquipmentGlossaryData[] AdverbStripEquipmentDatas;
+    /**
+     * 等级
+     */
+    private int equipmentLv;
+    /**
+     * 当前圣遗物最大等级
+     */
+    private int equipmentMaxLv;
+
+
 }
