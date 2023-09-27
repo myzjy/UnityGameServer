285f0a3225b96b46f6a1e8d0930d587c145c6d00
‘zjy’ <2910553808@qq.com>
2023-09-27 19:50:26 +0800

    部分修改

--- ampieta-game-java-common/src/main/java/com/gameServer/common/ormEntity/EquipmentUserDataOrmEntity.java
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/ormEntity/EquipmentUserDataOrmEntity.java
@@ -1,11 +1,13 @@
 package com.gameServer.common.ormEntity;
 
+import com.zfoo.orm.model.IEntity;
+
 /**
  * @author zjy
  * @version 1.0
  * @since 2023/9/26 17 22
  */
-public class EquipmentUserDataOrmEntity {
+public class EquipmentUserDataOrmEntity implements IEntity<Long> {
     /**
      * 所属玩家 UID
      */
@@ -19,4 +21,32 @@ public class EquipmentUserDataOrmEntity {
      */
     private int equipmentID;
 
+    @Override
+    public long id() {
+        return id;
+    }
+
+    public long getUid() {
+        return Uid;
+    }
+
+    public void setUid(long uid) {
+        Uid = uid;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getEquipmentID() {
+        return equipmentID;
+    }
+
+    public void setEquipmentID(int equipmentID) {
+        this.equipmentID = equipmentID;
+    }
 }
