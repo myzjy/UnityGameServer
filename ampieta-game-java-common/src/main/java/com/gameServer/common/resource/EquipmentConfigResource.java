e41f7aa8645e7893d63de4b8346c0c2ebb10cf6e
‘zjy’ <2910553808@qq.com>
2023-10-15 17:45:49 +0800

    数据表提交

---
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/resource/EquipmentConfigResource.java
@@ -0,0 +1,18 @@
+package com.gameServer.common.resource;
+
+import com.zfoo.storage.anno.Storage;
+
+/**
+ * @author zjy
+ * @version 1.0
+ * @since 2023/10/15 17 07
+ */
+@Storage
+public class EquipmentConfigResource {
+    private int quality;
+    private int lv1;
+    private int lv2;
+    private int lv3;
+    private int lv4;
+    private int lv5;
+}
