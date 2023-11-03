0a6325d790f4b3c5246f03842aceb9f1b2884d3d
‘zjy’ <2910553808@qq.com>
2023-11-03 17:24:51 +0800

    配置器

---
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/resource/EquipmentGrowthConfigResource.java
@@ -0,0 +1,37 @@
+package com.gameServer.common.resource;
+
+import com.zfoo.storage.anno.Id;
+import com.zfoo.storage.anno.Index;
+import com.zfoo.storage.anno.Storage;
+
+/**
+ * @author zjy
+ * @version 1.0
+ * @since 2023/11/3 17 20
+ */
+@Storage
+public class EquipmentGrowthConfigResource {
+    @Id
+    @Index
+    private int id;
+    /**
+     * 圣遗物位置
+     */
+    private int locationOfEquipmentType;
+    /**
+     * 位置的名字
+     */
+    private String posName;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLocationOfEquipmentType() {
+        return locationOfEquipmentType;
+    }
+
+    public String getPosName() {
+        return posName;
+    }
+}
