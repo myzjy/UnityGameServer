e3f87046f4ef39dc0f4d1c0be5ec82c61be6e015
‘zjy’ <2910553808@qq.com>
2023-11-06 14:35:38 +0800

    配置器

---
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/resource/EquipmentPrimaryConfigResource.java
@@ -0,0 +1,70 @@
+package com.gameServer.common.resource;
+
+import com.zfoo.storage.anno.Id;
+import com.zfoo.storage.anno.Index;
+import com.zfoo.storage.anno.Storage;
+
+/**
+ * @author zjy
+ * @version 1.0
+ * @since 2023/11/6 14 23
+ */
+
+@Storage
+public class EquipmentPrimaryConfigResource {
+    @Id
+    @Index
+    private int id;
+    /**
+     * 品阶
+     */
+    private int primaryQuality;
+    /**
+     * 圣遗物属性位置
+     */
+    private int growthPosInt;
+    /**
+     * 属性名字
+     */
+    private String growthPosName;
+    /**
+     * 1级初始属性
+     */
+    private int primaryGrowthInts;
+    /**
+     * 品阶的最大等级的最大属性值
+     */
+    private int primaryGrowthMaxInt;
+    /**
+     * 属性位置所属名字
+     */
+    private String primaryGrowthName;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrimaryQuality() {
+        return primaryQuality;
+    }
+
+    public int getGrowthPosInt() {
+        return growthPosInt;
+    }
+
+    public String getGrowthPosName() {
+        return growthPosName;
+    }
+
+    public int getPrimaryGrowthInts() {
+        return primaryGrowthInts;
+    }
+
+    public int getPrimaryGrowthMaxInt() {
+        return primaryGrowthMaxInt;
+    }
+
+    public String getPrimaryGrowthName() {
+        return primaryGrowthName;
+    }
+}
