965be2fa2f1dbc8bc82c968f1b948232f90aec49
‘zjy’ <2910553808@qq.com>
2023-11-26 17:54:39 +0800

    去掉部分

--- ampieta-game-home/src/test/java/com/gameServer/home/ApplicationTest.java
+++ ampieta-game-home/src/test/java/com/gameServer/home/ApplicationTest.java
@@ -1,18 +1,10 @@
 package com.gameServer.home;
 
 import com.gameServer.common.constant.TankDeployEnum;
-import com.gameServer.common.entity.PlayerUserEntity;
-import com.zfoo.orm.OrmContext;
-import com.zfoo.orm.anno.EntityCache;
-import com.zfoo.orm.anno.EntityCacheAutowired;
-import com.zfoo.orm.anno.Persister;
-import com.zfoo.orm.cache.IEntityCache;
-import com.zfoo.orm.model.IEntity;
 import com.zfoo.protocol.util.ThreadUtils;
-import com.zfoo.scheduler.util.TimeUtils;
+
 import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author zjy
@@ -21,13 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @Ignore
 public class ApplicationTest {
-    static {
-        TankDeployEnum.InitDefaultEnv();
-    }
+  static {
+    TankDeployEnum.InitDefaultEnv();
+  }
 
-    @Test
-    public void startApplication1() {
-        Application.main(null);
-        ThreadUtils.sleep(Long.MAX_VALUE);
-    }
+  @Test
+  public void startApplication1() {
+    Application.main(null);
+    ThreadUtils.sleep(Long.MAX_VALUE);
+  }
 }
