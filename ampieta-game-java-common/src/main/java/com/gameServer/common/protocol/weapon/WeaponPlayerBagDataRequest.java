5bc372ad80bee4d31be3aca7121eceaf4c52a6c0
zjy <2910553808@qq.com>
2024-03-19 23:18:34 +0800

    背包 协议 相关 controller 提交

---
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/weapon/WeaponPlayerBagDataRequest.java
@@ -0,0 +1,43 @@
+package com.gameServer.common.protocol.weapon;
+
+import com.zfoo.protocol.anno.Protocol;
+
+/**
+ * @author zjy
+ * @version 1.0
+ * @since 2024/3/19 23 05
+ */
+@Protocol(id = 1043)
+public class WeaponPlayerBagDataRequest {
+    /**
+     * 服务器 id？
+     */
+    private long serverId;
+    /**
+     * 玩家id
+     */
+    private long uid;
+
+    public long getServerId() {
+        return serverId;
+    }
+
+    public void setServerId(long serverId) {
+        this.serverId = serverId;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public void setUid(long uid) {
+        this.uid = uid;
+    }
+
+    public static WeaponPlayerBagDataRequest valueOf(long serverId, long uid) {
+        var data = new WeaponPlayerBagDataRequest();
+        data.serverId = serverId;
+        data.uid = uid;
+        return data;
+    }
+}
---
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/protocol/weapon/WeaponPlayerBagDataResponse.java
@@ -0,0 +1,13 @@
+package com.gameServer.common.protocol.weapon;
+
+import com.zfoo.protocol.anno.Protocol;
+
+/**
+ * @author zjy
+ * @version 1.0
+ * @since 2024/3/19 23 16
+ */
+@Protocol(id = 1044)
+public class WeaponPlayerBagDataResponse {
+    private long uid;
+}
