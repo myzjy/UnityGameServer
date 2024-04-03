38308ac27392f5dc5d1d737fe5ed66d4dd2d312c
zjy <1>
2024-04-03 18:06:19 +0800

    配置表

---
+++ ampieta-game-java-common/src/main/java/com/gameServer/common/resource/CharacterLevelConfigResource.java
@@ -0,0 +1,14 @@
+package com.gameServer.common.resource;
+
+import com.zfoo.storage.anno.Storage;
+
+/**
+ * 角色 20及 的 属性
+ *
+ * @author zjy
+ * @version 1.0
+ * @since 2024/4/2 18 12
+ */
+@Storage
+public class CharacterLevelConfigResource {
+}
